b35a9e18886a8f4d1469a7d10847db02d515ccff
cicelybeckford <cicelybeckford@users.noreply.github.com>
2020-02-10 18:17:14 -0500

    Tab Bar and Collection View

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Cicely Beckford on 2/10/20.
+//  Copyright © 2020 Cicely Beckford. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
